72010ac4ea138aad9d08cbb63020d5775890ab97
DhavalW <dwathare@gmail.com>
2023-09-20 11:17:05 +0530

    basic grid layout in place

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- @format -->
+<div class="row">
+	<div class="column centered"><h1>Welcome to SvelteKit</h1></div>
+</div>
+<div class="row">
+    <div class="column left p1"><input class="m0" /></div>
+</div>
+<div class="row">
+	<div class="column column-75 p1 centered">Col1</div>
+	<div class="column centered bordered p0" style="width:1px;" />
+	<div class="column column-25 p1 left">Col2</div>
+</div>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<style>
+	.p0 {
+		padding: 0;
+	}
+    .m0 {
+        margin:0;
+    }
+	.p1 {
+		padding: 1em;
+	}
+	.centered {
+		text-align: center;
+		align-items: center;
+	}
+	.left {
+		text-align: left;
+		align-items: left;
+	}
+	.bordered {
+		border: solid 1px;
+	}
+</style>
